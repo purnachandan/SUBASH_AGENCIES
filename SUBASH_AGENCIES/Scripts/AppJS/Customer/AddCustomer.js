28cfc4d5ec735a6dcf387b8820f8caaf08cb7461
Saichandan Muriki <sai.chandan06@gmail.com>
2016-01-23 13:31:04 +0530

    addind untracked files to repository

---
+++ SUBASH_AGENCIES/Scripts/AppJS/Customer/AddCustomer.js
@@ -0,0 +1,54 @@
+﻿app.controller('AddCustomer', function ($scope, $routeParams, $http, $window, $location) {
+    $scope.heading = "Add Customer";
+    $scope.id = $routeParams.customerid;
+    $scope.customer = {};
+    $scope.existingoutlet = false;
+    $scope.DisplayPin = function (cid) {
+        if (cid != null) {
+            angular.forEach($scope.city, function (key, value) {
+                if (key.CITYID == cid) {
+                    $scope.PINCODE = key.PINCODE;
+                }
+            });
+        }
+        else {
+            $scope.PINCODE = '';
+        }
+    }
+    if ($scope.id != 0) {
+        $scope.pullcustomer = $http.get('/Master/GetCustomerByID?id=' + $scope.id).success(function (data) {
+            $scope.customer = data;
+            $scope.DisplayPin($scope.customer.CITYID);
+        });
+        $scope.heading = "Update Customer";
+        $scope.existingoutlet = true;
+    }
+    else {
+        $scope.existingoutlet = false;
+    }
+    $scope.AddUpdateCustomer = function () {
+        var id = $scope.customer.ID;
+        $http.put('/Master/AddUpdateCustomer', $scope.customer).success(function (data) {
+            if (data == 1) {
+                if ($scope.id == 0) {
+                    $window.alert('Outlet: ' + $scope.customer.OUTLET_NAME + ' added successfully');
+                }
+                else {
+                    $window.alert('Outlet: ' + $scope.customer.OUTLET_NAME + ' updated successfully');
+                }
+            }
+            else {
+                $window.alert('Unable to add/update the Outlet: ' + $scope.customer.OUTLET_NAME);
+            }
+            $location.path('/');
+        }).error(function (data) {
+            $window.alert('Unable to add/update the customer: ' + $scope.customer.OUTLET_NAME);
+        });
+    }
+    $scope.PlayPinCode = function (cid) {
+        $scope.DisplayPin(cid);
+    }
+    $scope.ResetAddUpdateCustomer = function () {
+        $location.path('ViewCustomer');
+    }
+});
---
+++ SUBASH_AGENCIES/Scripts/AppJS/Customer/Customer.js
@@ -0,0 +1,35 @@
+﻿var app = angular.module('Customer', ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/ViewCustomer', {
+            templateUrl: '/PartialViews/Customer/ViewCustomer.html',
+            controller: 'ViewCustomer'
+        }).when('/AddCustomer/:customerid', {
+            templateUrl: '/PartialViews/Customer/AddCustomer.html',
+            controller: 'AddCustomer'
+        }).when('/', {
+            templateUrl: '/PartialViews/Customer/ViewCustomer.html',
+            controller: 'ViewCustomer'
+        });
+}).controller('customer', function ($scope, $http) {
+    $scope.beat = {};
+    $scope.category = {};
+    $scope.type = {};
+    $scope.city = {};
+    $scope.status = {};
+    $scope.category = $http.get('/Master/GetCategory').success(function (data) {
+        $scope.category = data;
+    });
+    $scope.type = $http.get('/Master/GetTypes').success(function (data) {
+        $scope.type = data;
+    });
+    $scope.beat = $http.get('/Master/GetBeat').success(function (data) {
+        $scope.beat = data;
+    });
+    $scope.city = $http.get('/Master/GetCity').success(function (data) {
+        $scope.city = data;
+    });
+    $scope.status = $http.get('/Master/GetStatus').success(function (data) {
+        $scope.status = data;
+    });
+});
